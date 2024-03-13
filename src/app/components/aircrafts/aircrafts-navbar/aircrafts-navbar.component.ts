49332451c21ec818f729f6443fa55b9b75f32be2
Claire AVILA <avila.claire@gmail.com>
2024-03-13 16:05:21 +0100

    ajout des évenements publish

--- src/app/components/aircrafts/aircrafts-navbar/aircrafts-navbar.component.ts
+++ src/app/components/aircrafts/aircrafts-navbar/aircrafts-navbar.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
+import { EventService } from 'src/app/services/event.service';
 import { AircraftsActionsTypes } from 'src/app/state/aircraft.state';
-
-
-
-
 
 @Component({
   selector: 'app-aircrafts-navbar',
@@ -16,24 +13,24 @@ export class AircraftsNavbarComponent implements OnInit {
 
   @Output() eventEmitter : EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  constructor(private eventService : EventService) { }
 
   ngOnInit(): void {
   }
   getAllAircrafts(){
-    this.eventEmitter.emit({type : AircraftsActionsTypes.GET_ALL_AIRCRAFTS,payload : null});
+    this.eventService.publishEvent({type : AircraftsActionsTypes.GET_ALL_AIRCRAFTS,payload : null});
   }
 
   getDesignAircrafts(){
-    this.eventEmitter.emit({type : AircraftsActionsTypes.GET_DESIGNED_AIRCRAFTS, payload : null});
+    this.eventService.publishEvent({type : AircraftsActionsTypes.GET_DESIGNED_AIRCRAFTS, payload : null});
   }
 
   getDeveloppementAircrafts(){
-    this.eventEmitter.emit({type : AircraftsActionsTypes.GET_DEVELOPMENT_AIRCRAFTS, payload : null});
+    this.eventService.publishEvent({type : AircraftsActionsTypes.GET_DEVELOPMENT_AIRCRAFTS, payload : null});
 
   }
   onSearch(value : any){
-    this.eventEmitter.emit({type : AircraftsActionsTypes.GET_SEARCH_AIRCRAFTS, payload : value})
+    this.eventService.publishEvent({type : AircraftsActionsTypes.GET_SEARCH_AIRCRAFTS, payload : value})
   }
 
 onActionEvent($event : any){
